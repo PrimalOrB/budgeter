34f3fd777f527af26db869c290014710258d14eb
Andrew Ogilvie <primalorb@gmail.com>
2022-05-08 12:36:44 -0400

    add rounding to bars

--- client/src/components/Charts/MultiMonthBudgetOverview.js
+++ client/src/components/Charts/MultiMonthBudgetOverview.js
@@ -4,6 +4,50 @@ import { Chart as ChartJS, CategoryScale, BarElement, LinearScale, PointElement,
 
 ChartJS.register( CategoryScale, BarElement, LinearScale, PointElement, LineElement, Title, Tooltip, Legend );
 
+const determineBorderRadius = (context) => {
+    const numDatasets = context.chart.data.datasets.length;
+    let showBorder = false;
+  
+    if (context.datasetIndex === numDatasets - 1) {
+      // Last dataset, always a yes
+      showBorder = true;
+    } else if (context.parsed.y !== 0) {
+      // We show a radius when we're the highest value in a positive stack
+      // or the lowest value in a negative stack
+      const sign = Math.sign(context.parsed.y);
+      let matches = false;
+  
+      // Look for any other non-0 values in the same stack that have the same sign
+      for (let i = context.datasetIndex + 1; i < numDatasets; i++) {
+        const val = context.parsed._stacks.y[i];
+        if (val && Math.sign(val) === sign) {
+          matches = true;
+          break;
+        }
+      }
+  
+      showBorder = !matches;
+    }
+  
+    if (!showBorder) {
+      return 0;
+    }
+  
+    let radius = 0;
+    if (context.parsed.y > 0) {
+      return {
+        topLeft: 25,
+        topRight: 25,
+      };
+    } else if (context.parsed.y < 0) {
+      return {
+        bottomLeft: 25,
+        bottomRight: 25,
+      };
+    }
+    return radius;
+  };
+
 const MultiMonthBudgetOverview = ( { data } ) => {
 
     const [ graphDataState, setGraphDataState ] = useState( null )
@@ -24,7 +68,8 @@ const MultiMonthBudgetOverview = ( { data } ) => {
                 backgroundColor: 'rgba(0, 128, 0, 0.25)',
                 borderWidth: 1,
                 borderColor: 'rgba(0, 128, 0, 1)',
-                stack: 'Stack 0'
+                stack: 'Stack 0',
+                borderRadius: determineBorderRadius,
             },
             {
                 type: 'bar',
@@ -35,7 +80,32 @@ const MultiMonthBudgetOverview = ( { data } ) => {
                 backgroundColor: 'rgba(200, 0, 0, 0.25)',
                 borderWidth: 1,
                 borderColor: 'rgba(200, 0, 0, 1)',
-                stack: 'Stack 0'
+                stack: 'Stack 0',
+                borderRadius: determineBorderRadius,
+            },
+            {
+                type: 'bar',
+                label: 'Income',
+                data: new Array( data.length ).fill().map( ( x, i ) => {
+                    return 0
+                } ),
+                backgroundColor: 'rgba(0, 128, 0, 0.5)',
+                borderWidth: 1,
+                borderColor: 'rgba(0, 128, 0, 1)',
+                stack: 'Stack 1',
+                borderRadius: determineBorderRadius,
+            },
+            {
+                type: 'bar',
+                label: 'Expense',
+                data: new Array( data.length ).fill().map( ( x, i ) => {
+                    return 0
+                } ),
+                backgroundColor: 'rgba(200, 0, 0, 0.5)',
+                borderWidth: 1,
+                borderColor: 'rgba(200, 0, 0, 1)',
+                stack: 'Stack 1',
+                borderRadius: determineBorderRadius,
             },
             {
                 type: 'line',
@@ -52,9 +122,11 @@ const MultiMonthBudgetOverview = ( { data } ) => {
         data.map( ( month, i ) => {
             labels.push( month.label )    
             datasets[0].data[i] = month.budgetedIncomeTotal
-            datasets[1].data[i] = - month.budgetedExpenseTotal               
-            datasets[2].data[i] += month.budgetedIncomeTotal  
-            return datasets[2].data[i] -= month.budgetedExpenseTotal  
+            datasets[1].data[i] = - month.budgetedExpenseTotal 
+            datasets[2].data[i] = month.incomeTotal
+            datasets[3].data[i] = - month.expenseTotal               
+            datasets[4].data[i] += month.incomeTotal  
+            return datasets[4].data[i] -= month.expenseTotal  
         })
 
         const newState = {
