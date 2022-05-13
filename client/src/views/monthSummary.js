678e5f57fe4a6cdbe728ed3b26c222449ca7954e
Andrew Ogilvie <primalorb@gmail.com>
2022-05-13 12:46:21 -0400

    expandable sections within monthly detail

--- client/src/views/monthSummary.js
+++ client/src/views/monthSummary.js
@@ -1,62 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { format } from 'date-fns'
 import { toCurrency } from '../utils/helpers'
 import { SingleMonthCategoryCost } from '../components/Charts'
-
+import { FaCaretDown, FaCaretUp } from 'react-icons/fa'
 
 const MonthSummary = ( { highlightMonthState, categories, transactions } ) => {
   
   const date = new Date( `20${Number( highlightMonthState.split('/')[1] )}`, Number( highlightMonthState.split('/')[0] ) - 1, 1 )
 
+  const [ expandedState, setExpandedState ] = useState( { income: false, expense: false } )
+
+  const expenseByMonth = transactions.filter( entry => entry.valueType === 'expense' )
+  const incomeByMonth = transactions.filter( entry => entry.valueType === 'income' )
+
   return (
     <section id="month-summary" >
       <h4 className="sub-container-description section-list-title">{ format( date, "MMMM yyyy" ) }</h4>
       <div className="dual-doughnut-container">
-        <SingleMonthCategoryCost valueType="expense" activeDate={ date } highlightMonthState={ highlightMonthState } categories={ categories.filter( category => category.categoryType === 'expense' ) } transactions={ transactions.filter( entry => entry.valueType === 'expense' ) } />
-        <SingleMonthCategoryCost valueType="income" activeDate={ date } highlightMonthState={ highlightMonthState } categories={ categories.filter( category => category.categoryType === 'income' ) } transactions={ transactions.filter( entry => entry.valueType === 'income' ) } />
+        <SingleMonthCategoryCost valueType="expense" activeDate={ date } highlightMonthState={ highlightMonthState } categories={ categories.filter( category => category.categoryType === 'expense' ) } transactions={ expenseByMonth } />
+        <SingleMonthCategoryCost valueType="income" activeDate={ date } highlightMonthState={ highlightMonthState } categories={ categories.filter( category => category.categoryType === 'income' ) } transactions={ incomeByMonth } />
       </div>
-      <ul className="section-list">
-        { transactions.length === 0 &&
-          <li className={ 'flex-transaction-line-sm border-bot-hightlight-1 f-valign' }>No Recent Transactions</li>
-        }
-        { transactions.map( entry => {
-          let type
-          if( entry.valueType === "income" && entry.value > 0){
-            type = 1
-          }
-          if( entry.valueType === "income" && entry.value < 0){
-            type = 3
-          }
-          if( entry.valueType === "expense" && entry.value > 0){
-            type = 0
-          }
-          if( entry.valueType === "expense" && entry.value < 0){
-            type = 2
-          }
 
-          return ( 
-            <li key={ `recent_${ entry._id }` } className={ 'flex-transaction-line-sm border-bot-hightlight-1 f-valign' }>
-              <div className="flex f1 wrap padding-top-sm">
-                <span className="margin-right-half colon bold f0 font-medium">
-                  { categories.filter( category => category._id === entry.categoryID )[0].title }
-                </span>
-                <span className='f1 font-medium'>
-                  { entry.title }
-                </span>
-                <span className="indent-1 italic font-small f-full padding-bottom-sm">
-                  { format( entry.createdAt, 'M/dd/yy' ) }
-                </span>
-              </div>
-              <span className={ `bold f0${ type === 0 ? ' negative' : ''}${ type === 1 ? ' positive' : ''}${ type === 2 ? ' credit' : ''}${ type === 3 ? ' reverse' : ''}` }>
-                { toCurrency( Math.abs( entry.value ) ) }
-              </span>
-              <span className='f0 initials-icon' style={{ backgroundColor: entry.userID.userColor ? `#${ entry.userID.userColor }` : '#BBBBBB' }}>
-                  { entry.userID.userInitials ? entry.userID.userInitials.toUpperCase() : entry.userID.email[0].toUpperCase() }
-                </span>
-            </li> )
-        })}
-      </ul>
-    
+      <ul className="monthly-group-detail border-b-l-rad-none border-b-r-rad-none border_b_none">        
+        <li className="flex nowrap flex-just-space-around f-full" onClick={ () => setExpandedState( { ...expandedState, expense: !expandedState.expense } )  }>
+          <span className="f0 margin-right-half">
+            { expandedState.expense ? FaCaretUp() : FaCaretDown() }
+          </span>
+          <span className="f1 bold">
+            Expenses
+          </span>
+        </li>
+        { expandedState.expense &&
+           expenseByMonth.map( entry => {
+             return (
+             <li key={ entry._id }>
+               { entry.value }
+             </li>
+             )
+           })
+        }
+      </ul>   
+
+      <ul className="monthly-group-detail border-t-l-rad-none border-t-r-rad-none">        
+        <li className="flex nowrap flex-just-space-around f-full" onClick={ () => setExpandedState( { ...expandedState, income: !expandedState.income } )  }>
+          <span className="f0 margin-right-half">
+            { expandedState.income ? FaCaretUp() : FaCaretDown() }
+          </span>
+          <span className="f1 bold">
+            Income
+          </span>
+        </li>
+        { expandedState.income &&
+           incomeByMonth.map( entry => {
+             return (
+             <li key={ entry._id }>
+               { entry.value }
+             </li>
+             )
+           })
+        }
+      </ul>   
     </section>
   )
 
