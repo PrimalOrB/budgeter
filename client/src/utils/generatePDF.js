253396ae56983af4ce504527e1c03ac48d45b039
Andrew Ogilvie <primalorb@gmail.com>
2023-05-17 14:36:08 -0400

    update data

--- client/src/utils/generatePDF.js
+++ client/src/utils/generatePDF.js
@@ -18,6 +18,15 @@ const generatePDF = ({
   const doc = new jsPDF();
   doc.showHead = "firstPage";
 
+  const subTotals = {
+    expensesShared: 0,
+    expensesSharedUser: 0,
+    incomeShared: 0,
+    incomeSharedUser: 0,
+    expensesIndividual: 0,
+    incomeIndividual: 0
+  }
+
   const expenseSharedCol = ["Date", "Title", "Total Expense", "Responsibility"];
   const expenseSharedRow = [];
   expensesShared.forEach((expense) => {
@@ -27,12 +36,13 @@ const generatePDF = ({
       toCurrency(expense.value),
       toCurrency(expense.value * user.portionSharedIncome),
     ];
+    subTotals.expensesShared += expense.value
+    subTotals.expensesSharedUser += (expense.value * user.portionSharedIncome)
     expenseSharedRow.push(expenseData);
   });
 
   const incomeSharedCol = ["Date", "Title", "Total Income", "Responsibility"];
   const incomeSharedRow = [];
-
   incomeShared.forEach((income) => {
     const incomeData = [
       format(income.createdAt, "M/dd/yy"),
@@ -40,29 +50,34 @@ const generatePDF = ({
       toCurrency(income.value),
       toCurrency(income.value * user.portionSharedIncome),
     ];
+    subTotals.incomeShared += income.value
+    console.log( 'before: ',subTotals.incomeSharedUser,)
+    subTotals.incomeSharedUser += (income.value * user.portionSharedIncome)
+    console.log( 'after: ',subTotals.incomeSharedUser)
     incomeSharedRow.push(incomeData);
   });
+
   const expenseCol = ["Date", "Title", "Individual Expense"];
   const expenseRow = [];
-
   expensesIndividual.forEach((expense) => {
     const expenseData = [
       format(expense.createdAt, "M/dd/yy"),
       expense.title,
       toCurrency(expense.value),
     ];
+    subTotals.expensesIndividual += expense.value
     expenseRow.push(expenseData);
   });
+
   const incomeCol = ["Date", "Title", "Individual Income"];
   const incomeRow = [];
-
   incomeIndividual.forEach((income) => {
     const incomeData = [
       format(income.createdAt, "M/dd/yy"),
       income.title,
-      toCurrency(income.value),
-      toCurrency(income.value * user.portionSharedIncome),
+      toCurrency(income.value)
     ];
+    subTotals.incomeIndividual += income.value
     incomeRow.push(incomeData);
   });
 
@@ -285,11 +300,27 @@ const generatePDF = ({
     },
   };
 
+  doc.autoTable({
+    columnStyles: titleStyle,
+    body: [[`${user.userInitials} - ${format(date, "M/dd/yy")} Summary`]],
+    startY: 20,
+    pageBreak: "auto",
+  });
+
+  if( user.userBalance !== 0 ){
+    doc.autoTable({
+        columnStyles: titleStyle,
+        body: [[`${ user.userBalance > 0 ? "You Owe " + toCurrency(user.userBalance) : "You Are Owed " + toCurrency(Math.abs(user.userBalance)) }`]],
+        startY: doc.lastAutoTable.finalY + 6,
+        pageBreak: "auto",
+      });
+  }
+
   if (expenseSharedRow.length) {
     doc.autoTable({
       columnStyles: titleStyle,
       body: [["Shared Expenses"]],
-      startY: 20,
+      startY: doc.lastAutoTable.finalY + 6,
       pageBreak: "auto",
     });
     doc.autoTable({
@@ -306,8 +337,8 @@ const generatePDF = ({
         [
           "",
           "Totals",
-          toCurrency(user.expensesShared / user.portionSharedExpenses),
-          toCurrency(user.expensesShared),
+          toCurrency(subTotals.expensesShared),
+          toCurrency(subTotals.expensesSharedUser),
         ],
       ],
       startY: doc.lastAutoTable.finalY,
@@ -336,8 +367,8 @@ const generatePDF = ({
         [
           "",
           "Totals",
-          toCurrency(user.incomeShared / user.portionSharedIncome),
-          toCurrency(user.incomeShared),
+          toCurrency(subTotals.incomeShared),
+          toCurrency(subTotals.incomeSharedUser),
         ],
       ],
       startY: doc.lastAutoTable.finalY,
@@ -363,7 +394,7 @@ const generatePDF = ({
     doc.autoTable({
       columnStyles: columnStyles3Total,
       body: [
-        ["", "Totals", toCurrency(user.expensesTotal - user.expensesShared)],
+        ["", "Totals", toCurrency(subTotals.expensesIndividual)],
       ],
       startY: doc.lastAutoTable.finalY,
       pageBreak: "auto",
@@ -387,7 +418,7 @@ const generatePDF = ({
     });
     doc.autoTable({
       columnStyles: columnStyles3Total,
-      body: [["", "Totals", toCurrency(user.incomeTotal - user.incomeShared)]],
+      body: [["", "Totals", toCurrency(subTotals.incomeIndividual)]],
       startY: doc.lastAutoTable.finalY,
       pageBreak: "auto",
     });
@@ -410,8 +441,6 @@ const generatePDF = ({
     });
   }
 
-
-  doc.text(`${user.userInitials} - ${format(date, "M/dd/yy")} Summary`, 14, 15);
 
   doc.save(`report_${ format(date, "M/dd/yy") }.pdf`);
 };
