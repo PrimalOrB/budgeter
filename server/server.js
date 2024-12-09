5e062f7559779bb2789583269084ad2b62815241
Andrew Ogilvie <primalorb@gmail.com>
2024-12-09 14:00:24 -0500

    store

--- server/server.js
+++ server/server.js
@@ -45,11 +45,11 @@ app.use(
 
 // app.use(express.static(path.join(__dirname, "../client/build/"))); // the same directory as below
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/dist/", "index.html"));
 });
 
 await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
