bde5f11b2f5d5545d171a39a177dab83f890d43e
Andrew Ogilvie <primalorb@gmail.com>
2024-12-09 13:53:12 -0500

    store

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ import { typeDefs, resolvers } from "./schemas/index.cjs";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-// const __dirname = path.dirname(__filename); // get the name of the directory
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const httpServer = http.createServer(app);
 const server = new ApolloServer({
@@ -44,6 +44,9 @@ app.use(
 );
 
 // app.use(express.static(path.join(__dirname, "../client/build/"))); // the same directory as below
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build/", "index.html"));
