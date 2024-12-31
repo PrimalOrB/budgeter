c92e1681292bd0db47921a779c156d2f231fc1cd
Andrew Ogilvie <primalorb@gmail.com>
2024-12-31 11:43:23 -0500

    fix

--- client/src/views/addBudget.jsx
+++ client/src/views/addBudget.jsx
@@ -1,98 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import { ActionButton } from '../components/Buttons'
-import { InlineTextInput, InlineTextareaInput, InlineListDisplay, InlineEmailInput } from '../components/Forms'
-import { InlineError, InlineNotification } from '../components/Notifications'
-import { useStoreContext } from '../utils/GlobalState'
-import { isEmail } from '../utils/helpers'
-import { useMutation } from '@apollo/client'
-import { CREATE_NEW_BUDGET } from '../utils/mutations'
-import { useNavigate  } from 'react-router-dom'
-import { Title } from '../components/Layout'
+import React, { useState, useEffect } from "react";
+import { ActionButton } from "../components/Buttons";
+import {
+  InlineTextInput,
+  InlineTextareaInput,
+  InlineListDisplay,
+  InlineEmailInput,
+} from "../components/Forms";
+import { InlineError, InlineNotification } from "../components/Notifications";
+import { useStoreContext } from "../utils/GlobalState";
+import { isEmail } from "../utils/helpers";
+import { useMutation } from "@apollo/client";
+import { CREATE_NEW_BUDGET } from "../utils/mutations";
+import { useNavigate } from "react-router-dom";
+import { Title } from "../components/Layout";
 
 const AddBudget = () => {
-  
-  const [ formInput, setFormInput ] = useState( { title: '', desc: '', email: '', owner: null, emails: [], error: null } ) 
-  
-  const [ state ] = useStoreContext();
+  const [formInput, setFormInput] = useState({
+    title: "",
+    desc: "",
+    email: "",
+    owner: "",
+    emails: [],
+    error: null,
+  });
 
-  const navigate = useNavigate ();
+  const [auditState, setAuditState] = useState({ pass: false });
 
-  useEffect(()=>{
-    if( state?.currentUser ){
-      setFormInput( {
-        ...formInput,
-        owner: state.currentUser.email
-      } )
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[ state ])
+  const [populated, setPopulated] = useState(false);
 
-  function validateForm( form ){
-    if( form.title === undefined || form.desc === undefined || form.owner === undefined ){
-      return false
-    }
-    if( form.title.length > 0 && form.desc.length > 0 && form.owner !== null && isEmail( form.owner) ){
-      return true
-    }
-    return false
-  }
+  const [state] = useStoreContext();
 
-  function sumbitForm(){
-    // check form validity
-    const valid = validateForm( formInput )
+  const navigate = useNavigate();
 
-    // if is valid, procees
-    if( valid ){
+  useEffect(() => {
+    if (state?.currentUser) {
       setFormInput({
         ...formInput,
-        error: null
+        owner: state.currentUser.email,
+      });
+      setPopulated(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state]);
+
+  function audit() {
+    const currentAuditState = { ...auditState },
+      currentFormState = { ...formInput };
+
+    const auditField = [];
+
+
+    const textRequired = ["title", "desc","owner"];
+    textRequired.map((field) => {
+      if( currentFormState[field] === undefined ){
+        currentAuditState[field] = false
+      } else {
+        currentAuditState[field] = currentFormState[field].length > 0;
+      }
+      auditField.push(currentAuditState[field]);
+    });
+
+    currentAuditState.pass =
+      auditField.filter((x) => x === true).length === auditField.length;
+
+    setAuditState({ ...currentAuditState });
+    return;
+  }
+
+  useEffect(() => {
+    if( populated ){
+      audit();
+    }
+  }, [formInput]);
+
+  function sumbitForm() {
+    // if is valid, procees
+    if (auditState.pass) {
+      setFormInput({
+        ...formInput,
+        error: null,
       });
 
       // send to submit
-      return processSumbit()
+      return processSumbit();
     }
     // return error
     setFormInput({
       ...formInput,
-      error: 'Failed form validation'
+      error: "Failed form validation",
     });
-    return console.log( 'failed' )
+    return console.log("failed");
   }
 
-  const [ processSumbit, { loading: createdLoading, error: createdError }] = useMutation(CREATE_NEW_BUDGET, {
-    variables: { 
-      input: {
-        owner: formInput.owner,
-        emails: formInput.emails,
-        title: formInput.title,
-        desc: formInput.desc
-      }
-    },
-    update: ( cache, data ) => {
-      try {
-        if( data ){
-          return navigate( `/budget/${ data.data.createBudget._id }` );
+  const [processSumbit, { loading: createdLoading, error: createdError }] =
+    useMutation(CREATE_NEW_BUDGET, {
+      variables: {
+        input: {
+          owner: formInput.owner,
+          emails: formInput.emails,
+          title: formInput.title,
+          desc: formInput.desc,
+        },
+      },
+      update: (cache, data) => {
+        try {
+          if (data) {
+            return navigate(`/budget/${data.data.createBudget._id}`);
+          }
+        } catch (e) {
+          console.error(createdError);
         }
-      } catch (e) {
-        console.error( createdError );
-      }
-  }
-  })
+      },
+    });
 
   return (
     <section className="full-container">
-      <Title text={ `Create New Budget` } />
+      <Title text={`Create New Budget`} />
       <form autoComplete="off">
-        <InlineTextInput prop={ 'title' } input={ formInput } setInput={ setFormInput } label={ 'Budget Title' }/>
-        <InlineTextareaInput prop={ 'desc' } input={ formInput } setInput={ setFormInput } label={ 'Description' }/>
-        <InlineListDisplay input={ formInput } setInput={ setFormInput } label={ 'Owners' }/>
-        <InlineEmailInput prop={ 'email' } input={ formInput } setInput={ setFormInput } label={ 'Add More Owners' } placeholder={ "Type or paste email addresses and press `Enter`" }/> 
-        { formInput.error && <InlineError text={ formInput.error }/> }
+        <InlineTextInput
+          prop={"title"}
+          input={formInput}
+          setInput={setFormInput}
+          label={"Budget Title"}
+          auditState={auditState}
+        />
+        <InlineTextareaInput
+          prop={"desc"}
+          input={formInput}
+          setInput={setFormInput}
+          label={"Description"}
+          auditState={auditState}
+        />
+        <InlineListDisplay
+          input={formInput}
+          setInput={setFormInput}
+          label={"Owners"}
+        />
+        <InlineEmailInput
+          prop={"email"}
+          input={formInput}
+          setInput={setFormInput}
+          label={"Add More Owners"}
+          placeholder={"Type or paste email addresses and press `Enter`"}
+        />
+        {formInput.error && <InlineError text={formInput.error} />}
       </form>
-      { createdLoading ? <InlineNotification text={ 'Submit processing' }/> :  <ActionButton action={ sumbitForm } text={ 'Submit' } additionalClass={ 'large-button' } /> }
+      {createdLoading ? (
+        <InlineNotification text={"Submit processing"} />
+      ) : (
+        <ActionButton
+          action={sumbitForm}
+          text={"Submit"}
+          additionalClass={"large-button"}
+          disabled={!auditState.pass}
+        />
+      )}
     </section>
-  )
-
+  );
 };
 
 export default AddBudget;
